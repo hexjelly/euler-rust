af08831e07e343bd881c4faef70a371574fe1d7a
Roger Andersen <hexjelly@hexjelly.com>
2019-10-27 10:35:18 +0100

    feat: solved problem 3

--- src/bin/euler-0003.rs
+++ src/bin/euler-0003.rs
@@ -1,7 +1,31 @@
-use euler::primes;
+use euler::primes::inc_sieve;
 
 /// The prime factors of 13195 are 5, 7, 13 and 29.
 ///
 /// What is the largest prime factor of the number 600851475143 ?
+fn largest_prime_factor(n: u64) -> u64 {
+  if n <= 1 {
+    return n;
+  }
+  let mut remain = n;
+  let mut primes = inc_sieve();
+  let mut factors = vec![];
 
-fn main() {}
+  while remain > 1 {
+    let next = primes.next().unwrap();
+    if remain % next == 0 {
+      factors.push(next);
+      remain /= next;
+    }
+  }
+  *factors.iter().max().unwrap()
+}
+
+fn main() {
+  println!("{}", largest_prime_factor(600_851_475_143))
+}
+
+#[test]
+fn euler_0003() {
+  assert_eq!(largest_prime_factor(13195), 29);
+}
